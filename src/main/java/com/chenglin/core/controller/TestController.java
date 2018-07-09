bb4322d235f3ea98eae11934b272de5f7e95cfad
chenglinX <chenglinx@chenglinXdeMacBook-Pro.local>
2018-07-09 20:56:55 +0800

    my mvc

---
+++ src/main/java/com/chenglin/core/controller/TestController.java
@@ -0,0 +1,39 @@
+package com.chenglin.core.controller;
+
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.chenglin.annotation.MyController;
+import com.chenglin.annotation.MyRequestMapping;
+import com.chenglin.annotation.MyRequestParam;
+
+@MyController
+@MyRequestMapping("/test")
+public class TestController {
+
+
+
+    @MyRequestMapping("/doTest")
+    public void test1(HttpServletRequest request, HttpServletResponse response,
+                      @MyRequestParam("param") String param){
+        System.out.println(param);
+        try {
+            response.getWriter().write( "doTest method success! param:"+param);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @MyRequestMapping("/doTest2")
+    public void test2(HttpServletRequest request, HttpServletResponse response){
+        try {
+            response.getWriter().println("doTest2 method success!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
